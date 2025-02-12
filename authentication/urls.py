107039872e1adf9cfb573606ec57f1c239d657b4
Mapacherama <48964493+Mapacherama@users.noreply.github.com>
2025-02-12 11:36:29 +0100

    Setup of Authentication

---
+++ authentication/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from authentication.views import RegisterView, login_view
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', login_view, name='login'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
