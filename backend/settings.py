2fdb4298382b296da30120d2e36e9be638be221a
Mapacherama <48964493+Mapacherama@users.noreply.github.com>
2025-02-12 08:24:23 +0100

    Fixing the url bug

--- backend/settings.py
+++ backend/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt",
     "corsheaders",
+    'django_extensions',
     "reflections",
     "django.contrib.admin",
     "django.contrib.auth",
--- reflections/urls.py
+++ reflections/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import ReflectionListCreateView
 
 urlpatterns = [
-    path("api/reflections/", ReflectionListCreateView.as_view(), name="reflection-list-create"),
+    path("reflections/", ReflectionListCreateView.as_view(), name="reflection-list-create"),
 ]
