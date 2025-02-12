8d67713a62a40542a7d3b0f1a3e07c06277f0228
Mapacherama <48964493+Mapacherama@users.noreply.github.com>
2025-02-12 07:40:36 +0100

    Setting up the Reflections needed for the view

--- reflections/views.py
+++ reflections/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from .models import Reflection
+from .serializers import ReflectionSerializer
 
-# Create your views here.
+class ReflectionListCreateView(generics.ListCreateAPIView):
+    serializer_class = ReflectionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Reflection.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
