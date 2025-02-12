27541469063de04f81d0e860ae7a018cfc606a19
Mapacherama <48964493+Mapacherama@users.noreply.github.com>
2025-02-12 07:37:07 +0100

    Fixing all the bugs, getting my first Django project up and running

--- reflections/models.py
+++ reflections/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Reflection(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Reflection by {self.user.username} on {self.created_at.strftime('%Y-%m-%d')}"
